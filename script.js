511755dd6e0c9b9bf41da604667931e62de0cf25
Janhvi Bhawar <janhvibhawar28@gmail.com>
2025-11-01 13:19:32 +0530

    Refactor and optimize animation functions

--- script.js
+++ script.js
@@ -28,37 +28,6 @@ function firstPageAnim(){
     })
 }
 
-// var timeout;
-// function miniMizeCircle(){
-//     //define default scale value
-//     var xscale = 1;
-//     var yscale = 1;
-
-//     var xprev = 0;
-//     var yprev = 0;
-
-//     window.addEventListener("mousemove", function(dets){
-//         clearTimeout(timeout);
-
-//         xscale = gsap.utils.clamp(0.8,1.2,dets.clientX - xprev);
-//         yscale = gsap.utils.clamp(0.8,1.2,dets.clientY - yprev);
-
-//         xprev = dets.clientX;
-//         yprev = dets.clientY;
-
-//        circleMouseFollower(xscale,yscale);
-//        timeout = setTimeout(function() {
-//         document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1,1)`;
-//        }, 100);
-//     });
-// }
-
-// function circleMouseFollower(xscale,yscale){
-//     window.addEventListener("mousemove", function(dets){
-//         document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
-//     });
-// }  
-
 
 function miniMizeCircle(){
     var xprev = 0;
@@ -170,4 +139,5 @@ document.querySelectorAll(".elem").forEach(elem => {
         });
     });
 });
+
 
